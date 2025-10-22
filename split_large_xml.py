fa6180f5fbfaf3a598ee7b1bb832e86522cbbb36
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2025-10-23 00:06:24 +0200

    Add XML split script to extract smaller test file for ISIL checks

---
+++ split_large_xml.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+
+#  Pfad zur Originaldatei
+xml_file = r"C:\xampp\htdocs\fhp-p2-data-quality\voebvoll-20241027.xml"
+
+# Name für die kleinere Test-Datei
+output_file = r"C:\xampp\htdocs\fhp-p2-data-quality\test-teil_1.xml"
+
+# anzhal der Datensätze in der Test-Datei
+max_records = 10000  # z. B. 10.000 Datensätze
+
+count = 0
+
+print(f"Starte das Aufteilen von {xml_file} ...")
+
+with open(output_file, "w", encoding="utf-8") as out:
+    out.write("<collection>\n")  # Root-Tag für MARCXML-Datei
+    for event, elem in ET.iterparse(xml_file, events=("end",)):
+        if elem.tag == "record":
+            out.write(ET.tostring(elem, encoding="unicode"))
+            count += 1
+            if count >= max_records:
+                break
+            elem.clear()
+    out.write("</collection>\n")
+
+print(f"✅ {count} Datensätze in {output_file} gespeichert.")
+print("Fertig!")
